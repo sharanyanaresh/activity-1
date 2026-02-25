f40430f98ea7f7e0fc3f83bf865301f12ea9ddab
sharanyanaresh <84068677+sharanyanaresh@users.noreply.github.com>
2026-02-25 15:28:58 +0530

    Create weather-analysis.py

---
+++ weather-analysis.py
@@ -0,0 +1,21 @@
+# Weather data for Gandhinagar (last 10 days)
+temperature = [34, 35, 33, 36, 37, 38, 36, 35, 34, 33]
+humidity = [45, 48, 50, 47, 46, 44, 43, 42, 41, 40]
+
+def average(data):
+    return sum(data) / len(data)
+
+def median(data):
+    data = sorted(data)
+    n = len(data)
+    mid = n // 2
+    if n % 2 == 0:
+        return (data[mid - 1] + data[mid]) / 2
+    else:
+        return data[mid]
+
+print("Temperature Average:", average(temperature))
+print("Temperature Median:", median(temperature))
+
+print("Humidity Average:", average(humidity))
+print("Humidity Median:", median(humidity))
