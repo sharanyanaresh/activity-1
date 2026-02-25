5459ad342acdb51005f9c20b2493df4a84917795
sharanyanaresh <84068677+sharanyanaresh@users.noreply.github.com>
2026-02-25 15:31:08 +0530

    Update weather-analysis.py

--- weather-analysis.py
+++ weather-analysis.py
@@ -2,6 +2,8 @@
 temperature = [34, 35, 33, 36, 37, 38, 36, 35, 34, 33]
 humidity = [45, 48, 50, 47, 46, 44, 43, 42, 41, 40]
 
+aqi = [92, 95, 90, 98, 100, 105, 102, 97, 94, 93]
+
 def average(data):
     return sum(data) / len(data)
 
@@ -19,3 +21,7 @@ print("Temperature Median:", median(temperature))
 
 print("Humidity Average:", average(humidity))
 print("Humidity Median:", median(humidity))
+
+print("AQI Average:", average(aqi))
+print("AQI Median:", median(aqi))
+
